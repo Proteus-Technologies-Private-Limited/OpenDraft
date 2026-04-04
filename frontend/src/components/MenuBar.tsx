2a9fc80bc1011cb9149bd363ebe7080831cdd76e
Kandarp Baghar <kandarp.baghar@proteustech.in>
2026-04-04 23:16:51 +0530

    Bump version to 0.8.0

--- frontend/src/components/MenuBar.tsx
+++ frontend/src/components/MenuBar.tsx
@@ -732,7 +732,7 @@ const MenuBar: React.FC<MenuBarProps> = ({ editor, onCollaborate, onJoinCollab, 
           <div className="dialog-header">About Open Draft</div>
           <div className="dialog-body about-body">
             <div className="about-title">Open Draft</div>
-            <div className="about-version">Version 0.7.1</div>
+            <div className="about-version">Version 0.8.0</div>
             <div className="about-tagline">Free, open-source screenwriting software</div>
 
             <div className="about-whats-new">
